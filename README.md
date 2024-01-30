8a91b8bc477b0b7d59bb259e88d23e4923e1dee2
Davide Bucher <>
2024-01-30 14:19:51 +0100

    add simple kml

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 pip3 install garmin-fit-sdk
 pip3 install simplekml
+
+
+record: ```{'timestamp': datetime.datetime(2023, 12, 30, 12, 9, 14, tzinfo=datetime.timezone.utc), 'position_lat': 550686408, 'position_long': 104778893, 'distance': 14919.59, 'enhanced_speed': 0.477, 'enhanced_altitude': 197.20000000000005, 'cycle_length16': 0.0, 141: 31, 'heart_rate': 97, 'cadence': 0, 'fractional_cadence': 0.0, 107: 0, 135: 28, 136: 97, 143: 41}```
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from garmin_fit_sdk import Decoder, Stream
 import simplekml
+from datetime import datetime, timedelta
+from simplekml import Kml
 
 # read .fit file
 stream = Stream.from_file("Activity.fit")
@@ -8,37 +10,99 @@ messages, errors = decoder.read()
 
 IDK_SHIT_CONVERSION = 11930465
 
-# create kml file
+coordinates = []
+when = []
+heartrate = []
+speed = []
+
+first_lat = None
+first_long = None
+first_when = None
+end_when = None
+first = True
+
+custom_times = []
+
+
+for record in messages["record_mesgs"]:
+    if first:
+        first_long = record["position_long"] / IDK_SHIT_CONVERSION
+        first_lat = record["position_lat"] / IDK_SHIT_CONVERSION
+        first_when = record["timestamp"]
+        end_when = first_when + timedelta(minutes=1)
+        first = False
+
+    custom_times.append(first_when + timedelta(seconds=0.01))
+
+    # print(record)
+    # print(record["timestamp"].strftime("%m/%d/%Y, %H:%M:%S"))
+    coordinates.append(
+        (
+            record["position_long"] / IDK_SHIT_CONVERSION,
+            record["position_lat"] / IDK_SHIT_CONVERSION,
+            record["enhanced_altitude"],
+        )
+    )
+    when.append(record["timestamp"].strftime("%d/%m/%Y, %H:%M:%S"))
+    heartrate.append(record["heart_rate"])
+    speed.append(int(record["enhanced_speed"]))
+
+# Create a KML object
 kml = simplekml.Kml()
 
-coords = []
+# Create a new point and style it
+pnt = kml.newpoint(name="Takeoff", coords=[(first_long, first_lat)])
+pnt.style.iconstyle.scale = 0.5
+
+# Create a tour and attach a playlist to it
+tour = kml.newgxtour(name="Play me!")
+playlist = tour.newgxplaylist()
+
+# Attach a gx:FlyTo to the playlist
+flyto = playlist.newgxflyto(gxduration=2)
+flyto.camera.longitude = first_long
+flyto.camera.latitude = first_lat
+flyto.camera.altitude = 10000
+flyto.camera.heading = -6.333
+flyto.camera.tilt = 5
+flyto.camera.roll = 30
+
 
 
 count = 0
-for record in messages["record_mesgs"]:
-    if count % 2 == 0:
-        coords.append(
-            (
-                record["position_long"] / IDK_SHIT_CONVERSION,
-                record["position_lat"] / IDK_SHIT_CONVERSION,
-                record["enhanced_altitude"],
-            )
-        )
-    # kml.newpoint(
-    #     name="tmp",
-    #     coords=[
-    #         (
-    #             record["position_long"] / IDK_SHIT_CONVERSION,
-    #             record["position_lat"] / IDK_SHIT_CONVERSION,
-    #             record["enhanced_altitude"],
-    #         )
-    #     ],
-    # )
-ls = kml.newlinestring(name="A LineString")
-ls.coords = coords
-ls.extrude = 0
-ls.altitudemode = simplekml.AltitudeMode.absolute
-ls.style.linestyle.width = 2
-ls.style.linestyle.color = simplekml.Color.yellow
+for triple in coordinates:
+    # Attach a gx:AnimatedUpdate to the playlist
+    animatedupdate = playlist.newgxanimatedupdate(gxduration=0.1)
 
-kml.save("para.kml")
+    partial_coords = [coordinates[count]]
+
+    ls = kml.newlinestring(name="line")
+    ls.extrude = 0
+    ls.altitudemode = simplekml.AltitudeMode.absolute
+    ls.style.linestyle.width = 2
+    ls.style.linestyle.color = simplekml.Color.yellow
+
+    ls.coords = partial_coords
+    animatedupdate.update.change = ls.__str__()
+
+    # if count == 5:
+    #     break
+
+    count = count + 1
+
+# Attach a gx:Wait to the playlist to give the gx:AnimatedUpdate time to finish
+wait = playlist.newgxwait(gxduration=1000)
+
+kml.save("test.kml")
+
+# # Create the KML document
+working_simple = Kml(name="working_simple", open=1)
+lis = working_simple.newlinestring(name="A LineString")
+lis.coords = coordinates
+lis.extrude = 0
+lis.altitudemode = simplekml.AltitudeMode.absolute
+lis.style.linestyle.width = 2
+lis.style.linestyle.color = simplekml.Color.yellow
+
+# # Save the kml to file
+working_simple.save("working_simple.kml")
