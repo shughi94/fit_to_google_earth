efc87ddb9a2bdad068a00292144de3f63f513c88
Shughi <shugar94@hotmail.com>
2024-08-28 09:56:45 +0200

    update code

--- main.py
+++ main.py
@@ -3,13 +3,23 @@ import simplekml
 from datetime import datetime, timedelta
 from simplekml import Kml
 
+
+# ------VARIABLES-----------
+
+PERCENTAGE_OF_LINE = 50
+ALTITUDE_ERROR = 0
+
+# -----------------------
+
+
+NO_IDEA_SHIT_CONVERSION = 11930465
+
 # read .fit file
 stream = Stream.from_file("Activity.fit")
 decoder = Decoder(stream)
 messages, errors = decoder.read()
 
-IDK_SHIT_CONVERSION = 11930465
-
+# envs
 coordinates = []
 when = []
 heartrate = []
@@ -21,79 +31,40 @@ first_when = None
 end_when = None
 first = True
 
-custom_times = []
+custom_times = [] #TODO
 
+
+percentage = int(len(messages["record_mesgs"]) * (PERCENTAGE_OF_LINE / 100))
+count = 0
 
 for record in messages["record_mesgs"]:
-    if first:
-        first_long = record["position_long"] / IDK_SHIT_CONVERSION
-        first_lat = record["position_lat"] / IDK_SHIT_CONVERSION
-        first_when = record["timestamp"]
-        end_when = first_when + timedelta(minutes=1)
-        first = False
+    try:
+        if first:
+            first_long = record["position_long"] / NO_IDEA_SHIT_CONVERSION
+            first_lat = record["position_lat"] / NO_IDEA_SHIT_CONVERSION
+            first_when = record["timestamp"]
+            end_when = first_when + timedelta(minutes=1)
+            first = False
 
-    custom_times.append(first_when + timedelta(seconds=0.01))
+        custom_times.append(first_when + timedelta(seconds=0.01))
 
-    # print(record)
-    # print(record["timestamp"].strftime("%m/%d/%Y, %H:%M:%S"))
-    coordinates.append(
-        (
-            record["position_long"] / IDK_SHIT_CONVERSION,
-            record["position_lat"] / IDK_SHIT_CONVERSION,
-            record["enhanced_altitude"],
+        # print(record)
+        # print(record["timestamp"].strftime("%m/%d/%Y, %H:%M:%S"))
+        coordinates.append(
+            (
+                record["position_long"] / NO_IDEA_SHIT_CONVERSION,
+                record["position_lat"] / NO_IDEA_SHIT_CONVERSION,
+                record["enhanced_altitude"] + ALTITUDE_ERROR,
+            )
         )
-    )
-    when.append(record["timestamp"].strftime("%d/%m/%Y, %H:%M:%S"))
-    heartrate.append(record["heart_rate"])
-    speed.append(int(record["enhanced_speed"]))
-
-# Create a KML object
-kml = simplekml.Kml()
-
-# Create a new point and style it
-pnt = kml.newpoint(name="Takeoff", coords=[(first_long, first_lat)])
-pnt.style.iconstyle.scale = 0.5
-
-# Create a tour and attach a playlist to it
-tour = kml.newgxtour(name="Play me!")
-playlist = tour.newgxplaylist()
-
-# Attach a gx:FlyTo to the playlist
-flyto = playlist.newgxflyto(gxduration=2)
-flyto.camera.longitude = first_long
-flyto.camera.latitude = first_lat
-flyto.camera.altitude = 10000
-flyto.camera.heading = -6.333
-flyto.camera.tilt = 5
-flyto.camera.roll = 30
-
-
-
-count = 0
-for triple in coordinates:
-    # Attach a gx:AnimatedUpdate to the playlist
-    animatedupdate = playlist.newgxanimatedupdate(gxduration=0.1)
-
-    partial_coords = [coordinates[count]]
-
-    ls = kml.newlinestring(name="line")
-    ls.extrude = 0
-    ls.altitudemode = simplekml.AltitudeMode.absolute
-    ls.style.linestyle.width = 2
-    ls.style.linestyle.color = simplekml.Color.yellow
-
-    ls.coords = partial_coords
-    animatedupdate.update.change = ls.__str__()
-
-    # if count == 5:
-    #     break
-
+        when.append(record["timestamp"].strftime("%d/%m/%Y, %H:%M:%S"))
+        heartrate.append(record["heart_rate"])
+        speed.append(int(record["enhanced_speed"]))
+    except:
+        continue
     count = count + 1
-
-# Attach a gx:Wait to the playlist to give the gx:AnimatedUpdate time to finish
-wait = playlist.newgxwait(gxduration=1000)
-
-kml.save("test.kml")
+    if count > percentage:
+        break
 
 # # Create the KML document
 working_simple = Kml(name="working_simple", open=1)
